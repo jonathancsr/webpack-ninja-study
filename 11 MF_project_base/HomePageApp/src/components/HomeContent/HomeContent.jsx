4d084a5c88580efb222e922fef62026ffc589443
jonathancsr <jonathancsrr@gmail.com>
2022-06-03 15:58:19 -0300

    feat: add movies api call

--- 11 MF_project_base/HomePageApp/src/components/HomeContent/HomeContent.jsx
+++ 11 MF_project_base/HomePageApp/src/components/HomeContent/HomeContent.jsx
@@ -7,8 +7,11 @@ const dummyItem = [{name:"Dummy Movie"}]
 const HomeContent = (props) => {
   const [movies, setMovies] = useState(dummyItem);
 
-  useEffect(() => {
+  useEffect(async () => {
     // Add the logic to load the movies from server and set to the state
+    const response = await fetch("http://localhost:5555/movies");
+    const data = await response.json();
+    console.log(data);
   }, []);
 
   const movieClicked = (item) => {
