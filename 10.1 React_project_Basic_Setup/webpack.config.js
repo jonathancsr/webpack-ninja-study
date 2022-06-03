f68d463aaa598326ca30b92ae361723ca486fd2a
jonathancsr <jonathancsrr@gmail.com>
2022-06-03 15:49:01 -0300

    feat: Micro Front with Webpack MF

--- 10.1 React_project_Basic_Setup/webpack.config.js
+++ 10.1 React_project_Basic_Setup/webpack.config.js
@@ -1,68 +1,99 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, "src/index.js"),
-    output: {
-        filename: "[name].bundle.js",
-        path:  path.resolve(__dirname, "dist")
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new EslintPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            filename: "index.html"
-        }),
-        new MiniCssExtractPlugin(),
-    ],
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, "dist"),
-        },
-        port: 9000,
-        open: true,
-    },
-    module:{
-        rules:[
-            {
-                test: /\.(js|jsx)$/,
-                include: path.resolve(__dirname, "src"),
-                exclude: path.resolve(__dirname, "node_modules"),
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options:{
-                            presets: [
-                                [
-                                    "@babel/preset-env",{
-                                        targets: "defaults",
-                                    }
-                                ],
-                                "@babel/preset-react",
+    port: 9000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: path.resolve(__dirname, 'node_modules'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env', {
+                    targets: 'defaults',
+                  },
+                ],
+                '@babel/preset-react',
 
-                            ]
-                        }
-                    }
-                ]
+              ],
             },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
+          },
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true,
             },
-            {
-                test: /\.(sass|scss)$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [
+                ['postcss-preset-env', {}],
+              ],
             },
-            {
-                test: /\.(jpg|png|gif|jpeg)$/,
-                type: "asset/resource",
+          },
+        }],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['postcss-preset-env', {}],
+                ],
+              },
             },
-        ]
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
     },
-    optimization:{
-        splitChunks: {
-            chunks: "all",
-        }
-    }
-}
+  },
+};
